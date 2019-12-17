b0eead0f8c845683121ae698b809f682abb45799
Bo Wen <wenbostar@gmail.com>
2019-12-17 12:32:44 -0600

    Add docker for PGA

---
+++ install.R
@@ -0,0 +1,6 @@
+# Install the development version from GitHub:
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+install.packages("remotes")
+BiocManager::install("wenbostar/PGA")
+
